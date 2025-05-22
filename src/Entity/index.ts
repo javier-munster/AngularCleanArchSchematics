0679b8d6686ed04deb51d77d848ab129a55bf74c
Javier Munster <javier.munster@nanoleaf.me>
2025-05-21 21:34:25 -0400

    chg: updated clean arch modules to use Effect and Schema

--- src/Entity/index.ts
+++ src/Entity/index.ts
@@ -12,15 +12,18 @@ import { normalize, strings } from '@angular-devkit/core';
 interface EntityOptions {
     name: string;
     feature: string;
+    flat?: boolean;
 }
 
 export function entity(options: EntityOptions): Rule {
     const opts = {
         name: strings.classify(options.name),
         feature: strings.classify(options.feature),
+        flat: options.flat ?? false,
     };
 
-    const targetPath = normalize(`src/app/${opts.feature}/Domain/Entities`);
+    const folder = opts.flat ? '' : `/${opts.name}Entity`;
+    const targetPath = normalize(`src/app/${opts.feature}/Domain/Entities${folder}`);
 
     const templateSource = apply(url('./files'), [
         applyTemplates({
